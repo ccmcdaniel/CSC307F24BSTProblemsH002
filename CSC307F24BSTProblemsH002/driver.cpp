8d7aa50919142d80a14f46721c70864a7b75edd8
Chad McDaniel <93947281+ccmcdaniel@users.noreply.github.com>
2024-10-21 11:46:10 -0500

    added test bench for insertion

---
+++ CSC307F24BSTProblemsH002/driver.cpp
@@ -0,0 +1,13 @@
+#include "BinarySearchTree.h"
+
+int main()
+{
+	BinarySearchTree my_bst;
+
+	my_bst.Insert(48);
+	my_bst.Insert(12);
+	my_bst.Insert(96);
+	my_bst.Insert(77);
+	my_bst.Insert(34);
+	my_bst.Insert(64);
+}
